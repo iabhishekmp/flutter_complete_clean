3561d7c5d362f5ecf683cede45760b274788d1c0
ABHISHEK <iabhishekmp@gmail.com>
2024-01-27 18:40:12 +0530

    [ Improved ] cli_util used

--- bin/flutter_complete_clean.dart
+++ bin/flutter_complete_clean.dart
@@ -2,27 +2,41 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:cli_util/cli_logging.dart';
+
 Future<void> main(List<String> arguments) async {
+  //? logger
+  final isVerbose = arguments.contains('-v');
+  final logger = isVerbose ? Logger.verbose() : Logger.standard();
+
+  //? current directory
   final currentDir = Directory.current;
-  print("currentDir: $currentDir");
+  logger.stdout("currentDir: $currentDir");
+
+  //? getting all the flutter projects
   final files = currentDir
       .listSync(recursive: true, followLinks: false)
       .whereType<File>()
       .where((e) => e.path.endsWith('pubspec.yaml'));
 
-  // add all the process to the list
+  //? cleaning all the flutter projects
   for (final file in files) {
+    // log the directory
     final dir = file.parent;
-    print(dir.path);
+
+    // starting flutter clean
     final process = await Process.start(
       "flutter",
       ["clean"],
       workingDirectory: dir.path,
       runInShell: true,
     );
-    process.stdout.transform(utf8.decoder).forEach(print);
+    process.stdout.transform(utf8.decoder).forEach(logger.trace);
+
+    var progress = logger.progress(dir.path);
     await process.exitCode;
+    progress.finish(showTiming: true);
   }
 
-  print('Done');
+  logger.stdout('Done');
 }
