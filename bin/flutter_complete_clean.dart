e3bc7c566cfd44b2f8b9c69b6d3e55bca343e9fa
ABHISHEK <iabhishekmp@gmail.com>
2024-01-27 19:40:07 +0530

    [ Improved ] - Saved Size in MB logged

--- bin/flutter_complete_clean.dart
+++ bin/flutter_complete_clean.dart
@@ -4,39 +4,62 @@ import 'dart:io';
 
 import 'package:cli_util/cli_logging.dart';
 
-Future<void> main(List<String> arguments) async {
-  //? logger
-  final isVerbose = arguments.contains('-v');
-  final logger = isVerbose ? Logger.verbose() : Logger.standard();
+late final Logger logger;
 
-  //? current directory
+Future<void> main(List<String> arguments) async {
+  final isVerbose = arguments.contains('-v');
+  logger = isVerbose ? Logger.verbose() : Logger.standard();
+
   final currentDir = Directory.current;
   logger.stdout("currentDir: $currentDir");
 
-  //? getting all the flutter projects
-  final files = currentDir
-      .listSync(recursive: true, followLinks: false)
-      .whereType<File>()
-      .where((e) => e.path.endsWith('pubspec.yaml'));
+  final beforeSize = await currentDir.getSize;
+
+  final files = currentDir.getAllPubspec;
 
   //? cleaning all the flutter projects
   for (final file in files) {
-    // log the directory
     final dir = file.parent;
 
-    // starting flutter clean
-    final process = await Process.start(
-      "flutter",
-      ["clean"],
-      workingDirectory: dir.path,
-      runInShell: true,
-    );
-    process.stdout.transform(utf8.decoder).forEach(logger.trace);
+    final process = await dir.flutterClean();
 
     var progress = logger.progress(dir.path);
     await process.exitCode;
     progress.finish(showTiming: true);
   }
 
-  logger.stdout('Done');
+  //? log sizes
+  final afterSize = await currentDir.getSize;
+  logger.stdout('BeforeSize: $beforeSize MB, AfterSize: $afterSize MB');
+  logger.stdout('Saved: ${beforeSize - afterSize} MB');
+}
+
+extension DirectoryExtension on Directory {
+  Iterable<File> get getAllPubspec =>
+      listSync(recursive: true, followLinks: false)
+          .whereType<File>()
+          .where((e) => e.path.endsWith('pubspec.yaml'));
+
+  Future<Process> flutterClean() async {
+    final process = await Process.start(
+      "flutter",
+      ["clean"],
+      workingDirectory: path,
+      runInShell: true,
+    );
+    process.stdout.transform(utf8.decoder).forEach(logger.trace);
+    return process;
+  }
+
+  /// in MB
+  Future<double> get getSize async {
+    int totalSize = 0;
+    listSync(recursive: true, followLinks: false)
+        .forEach((FileSystemEntity entity) {
+      if (entity is File) {
+        totalSize += entity.lengthSync();
+      }
+    });
+    return (totalSize / 1000000);
+  }
 }
